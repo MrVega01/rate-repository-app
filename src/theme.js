9f3d53a2592d604553e98baee48e78d57f28aea2
MrVega01 <ignaciovega200301@gmail.com>
2023-06-27 22:10:18 -0400

    Add: Global app theme data

---
+++ src/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  colors:{
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#049',
+    white: '#fefefe'
+  },
+  fontSizes:{
+    body: 14,
+    subheading: 16,
+    sub: 12
+  },
+  fonts: {
+    main: 'System'
+  },
+  fontWeights: {
+    normal: 400,
+    bold: 700,
+  }
+}
